c84495c535932a8e76435523d7b3c81858ab1c37
Merline Belizaire <merline@noukod.com>
2019-01-17 16:51:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,47 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
+    set :views, Proc.new { File.join(root, "../views/") }
 
-  configure do
-    enable :sessions
-    set :session_secret, "secret"
-  end
+    configure do
+      enable :sessions
+      set :session_secret, "secret"
+    end
 
-  get '/' do
-    erb :home
-  end
+    get '/' do
+      erb :home
+    end
 
-  get '/registrations/signup' do
-    erb :'/registrations/signup'
-  end
+    get '/registrations/signup' do
+      erb :'/registrations/signup'
+    end
 
-  post '/registrations' do
+    post '/registrations' do
+      @user = User.create name: params[:name], email: params[:email], password: params[:password]
+      session[:id] = @user.id
 
-    redirect '/users/home'
-  end
+      redirect '/users/home'
+    end
 
-  get '/sessions/login' do
-    erb :'sessions/login'
-  end
+    get '/sessions/login' do
+      erb :'sessions/login'
+    end
 
-  post '/sessions' do
+    post '/sessions' do
+      @user = User.find_by(email: params[:email], password: params[:password])
+      unless @user.nil?
+        session[:id] = @user.id
+        redirect '/users/home'
+      end
+      redirect '/sessions/login'
+    end
 
-    redirect '/users/home'
-  end
-
-  get '/sessions/logout' do 
-
-    redirect '/'
-  end
+    get '/sessions/logout' do
+      session.clear
+      redirect '/'
+    end
 
   get '/users/home' do
-
+      @user = User.find(session[:id])
     erb :'/users/home'
   end
 
