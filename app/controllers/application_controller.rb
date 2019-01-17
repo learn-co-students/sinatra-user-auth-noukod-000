7f56df3f6ab1276addb933c2dfd15b42004cabdc
Merline Belizaire <merline@noukod.com>
2019-01-17 16:52:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,42 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    configure do
-      enable :sessions
-      set :session_secret, "secret"
-    end
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
 
-    get '/' do
-      erb :home
-    end
+  get '/' do
+    erb :home
+  end
 
-    get '/registrations/signup' do
-      erb :'/registrations/signup'
-    end
+  get '/registrations/signup' do
 
-    post '/registrations' do
-      @user = User.create name: params[:name], email: params[:email], password: params[:password]
-      session[:id] = @user.id
+    erb :'/registrations/signup'
+  end
 
+  post '/registrations' do
+    @user = User.find_by(email: params[:email])
+    session[:user_id] = @user.id
+
+    redirect '/users/home'
+  end
+
+  get '/sessions/login' do
+
+    # the line of code below render the view page in app/views/sessions/login.erb
+    erb :'sessions/login'
+  end
+
+  post '/sessions' do
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
       redirect '/users/home'
     end
-
-    get '/sessions/login' do
-      erb :'sessions/login'
-    end
-
-    post '/sessions' do
-      @user = User.find_by(email: params[:email], password: params[:password])
-      unless @user.nil?
-        session[:id] = @user.id
-        redirect '/users/home'
-      end
-      redirect '/sessions/login'
-    end
+    redirect '/sessions/login'
+  end
 
     get '/sessions/logout' do
       session.clear
