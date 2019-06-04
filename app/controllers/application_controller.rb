9d391e2e730c4c40652a68044bad24086fe35c11
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-06-04 14:48:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,29 +11,35 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
+  # render the sign-up form view.
   get '/registrations/signup' do
     erb :'/registrations/signup'
   end
 
+  # responsible for handling the POST request that is sent when a user hits 'submit' on the sign-up form
   post '/registrations' do
 
     redirect '/users/home'
   end
 
+  # is responsible for rendering the login form
   get '/sessions/login' do
     erb :'sessions/login'
   end
 
+  # This route is responsible for receiving the POST request that gets sent when a user hits 'submit' on the login form.
   post '/sessions' do
 
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  # This route is responsible for logging the user out by clearing the session hash.
+  get '/sessions/logout' do
 
     redirect '/'
   end
 
+  # This route is responsible for rendering the user's homepage view.
   get '/users/home' do
 
     erb :'/users/home'
